9baa9cb7916ef063f8d3326f5ddd889e44f4c785
CobraMJD <42187314+CobraMJD@users.noreply.github.com>
2018-09-04 23:50:04 -0300

    Update viewport.cpp

--- viewport.cpp
+++ viewport.cpp
@@ -1,17 +1,61 @@
 #include "polygon.cpp"
 
+#define Spacing 25 //tem q ser igual ao Spacing da window
 
 using namespace std;
 
 class Viewport {
     public:
     Polygon::point min, max;
-    int pointWidth = 4;
+    int pointWidth = 2;
     
     Viewport() {
-        min.x = 0;
-        min.y = 0;
-        max.x = 400;
-        max.y = 400;
+        min.x = 10;
+        min.y = 10;
+        max.x = 390;
+        max.y = 390;
+    }
+    void reset() { //deve ter um jeito melhor
+        min.x = 10;
+        min.y = 10;
+        max.x = 390;
+        max.y = 390;
+    }
+    void zoom(bool z) {
+        if (!z)  {
+            max.x = max.x + Spacing;
+            min.x = min.x - Spacing;
+            max.y = max.y + Spacing;
+            min.y = min.y - Spacing;
+        }
+        else {
+            max.x = max.x - Spacing;
+            min.x = min.x + Spacing;
+            max.y = max.y - Spacing;
+            min.y = min.y + Spacing;
+        }
+    }
+    
+    void moveUp(){
+        //move(0,-Spacing);
+        min.y = min.y + Spacing;
+        max.y = max.y + Spacing;
+    }
+    void moveLeft (){
+        //move(Spacing,0);
+        min.x = min.x - Spacing;
+        max.x = max.x - Spacing;
+    }
+    
+    void moveRight (){
+        //move(-Spacing,0);
+        min.x = min.x + Spacing;
+        max.x = max.x + Spacing;
+    }
+    
+    void moveDown (){
+        //move(0,Spacing);
+        min.y = min.y - Spacing;
+        max.y = max.y - Spacing;
     }
 };
